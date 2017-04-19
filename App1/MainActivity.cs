848a5af7dac71cf487d8d2597cb6c163cdc555fc
Faizan M <mfaizanm@deloitte.com>
2017-04-19 13:28:53 +0530

    Add project files.

---
+++ App1/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            MobileCenter.Start("374739fe-487d-47e1-8c1d-307d7ef79a2f",typeof(Analytics), typeof(Crashes));
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
