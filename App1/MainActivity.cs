54f267e4fdd80aafd968cde1fd0b76df62716699
faizan21 <faizan2131993@gmail.com>
2017-04-19 15:05:50 +0530

    fifth ccommit

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -14,7 +14,7 @@ namespace App1
         {
             base.OnCreate(bundle);
             MobileCenter.Start("374739fe-487d-47e1-8c1d-307d7ef79a2f",typeof(Analytics), typeof(Crashes)); //sample
-            // Set our view from the "main" layout resource
+            // Set our view from the "main" layout resource sample
             // SetContentView (Resource.Layout.Main);
         }
     }
