e037b1253f3610790fa506f2deb74b73e062d82f
faizan21 <faizan2131993@gmail.com>
2017-04-20 14:40:22 +0530

    tenth commit

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -15,8 +15,11 @@ namespace App1
         {
             base.OnCreate(bundle);
             MobileCenter.Start("374739fe-487d-47e1-8c1d-307d7ef79a2f",typeof(Analytics), typeof(Crashes), typeof(Distribute)); //sample
-            MobileCenter.SetLogUrl("http://xyz.comg");
-            throw new MobileCenterException("Crashed");
+            var x=crash().ToString();
+            object crash() {
+                return null;
+            }
+            
           //  throw new System.StackOverflowException("this is crash");
             // Set our view from the "main" layout resource sample
             // SetContentView (Resource.Layout.Main);
